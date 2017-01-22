6b7a9f10a50cf9a96c5dea5ca6a2282e137c1db7
Sertan Senturk <contact@sertansenturk.com>
2017-01-22 01:32:17 +0100

    First version of test_annotations This version checks whether all annotations are within a given cent-interval between each other, or not. If not, it gives an assertion error. This version does not handle cases where a recording has tonic frequency changes inside the performance yet.

--- unittests/validate_annotations.py
+++ unittests/validate_annotations.py
@@ -1,9 +1,27 @@
 from morty.evaluator import Evaluator
 import json
+import numpy as np
 
 
 def test_annotations():
-    annos = json.load(open('annotations.json'))
     evaluator = Evaluator()
 
-    print("yay")
+    # load the tonic annotations
+    annos = json.load(open('annotations.json'))
+    eval_dict = {}
+    mismatch_mbid = []
+    for rm, av in annos.items():
+        # evaluate
+        vals = [ava['value'] for ava in av['annotations']]
+        evals = np.zeros((len(vals), len(vals)), dtype=bool)
+        for i1, v1 in enumerate(vals):
+            for i2, v2 in enumerate(vals):
+                evals[i1, i2] = evaluator.evaluate_tonic(v1, v2)['tonic_eval']
+
+        eval_dict[rm] = {'all': evals.all(), 'eval': evals}
+        if not evals.all():
+            print('http://dunya.compmusic.upf.edu/makam/recording/' + rm)
+            mismatch_mbid.append(rm)
+
+    assert not mismatch_mbid, "There are inconsistent annotations in %d " \
+                              "recordings" % len(mismatch_mbid)
